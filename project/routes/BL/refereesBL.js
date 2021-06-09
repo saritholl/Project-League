374d965ed9f1314c426690b1772a793663940d57
tom dugma <dugma@post.bgu.ac.il>
2021-06-09 13:38:29 +0300

    9.6.21 #3

--- project/routes/BL/refereesBL.js
+++ project/routes/BL/refereesBL.js
@@ -1,3 +1,4 @@
+const { Int } = require("mssql");
 const Errors = require("../../errors");
 
 
@@ -7,11 +8,36 @@ class refereesBL {
     this.refereesDAL = refereesDAL
   }
 
+  
+
   async addReferee(refereeName,refereeType,refereeStatus) {
+
+    if (refereeName == null || refereeType == null || refereeStatus == null ) {
+      throw {
+        "message": Errors.PARAMETER_NULL,
+        "code": 404
+      }
+    }
+
+    if (!(Number.isInteger(refereeType)) || !(Number.isInteger(refereeStatus))  || !(typeof refereeName === 'string')){
+      throw {
+        "message": Errors.WRONG_INSTANCE_OF_PARAMETER,
+        "code": 400
+      }
+    }
+
+    if (refereeType < 0 || refereeStatus < 0 ) {
+      throw {
+        "message": Errors.INVALID_PARAMETER,
+        "code": 400
+      }
+    }
+
+
     if (!(refereeName.split(" ").length == 2)) {
       throw {
         "message": Errors.INVALID_REFEREE_NAME,
-        "code": 400
+        "code": 404
       }
     }
 
